3a4a4f4ffb20bcad6f45ec700514dc77cf9ef55e
FredericoTakayama <f.takayama2014@gmail.com>
2018-07-19 22:38:32 -0300

    Blackjack - Initial commit
    Signed-off-by: FredericoTakayama <f.takayama2014@gmail.com>

---
+++ blackjack.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# blackjack - project2 study python
+
+import os
+
+def gretting_display():
+    print('_____________________________________')
+    print('___________  blackjack  _____________')
+    print('_____________________________________')
+    print('')
+
+if __name__ == "__main__":    
+    gretting_display()
